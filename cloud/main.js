041d1101621313654e8702b1d6c25544f88a7524
Matthew Olson <Olson@Matthews-MacBook-Air-2.local>
2016-06-12 19:17:56 -0600

    Updated cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,26 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("smsLoginVerification", function(request, response) {
+  var auth_id = "MAMJCXYTDMNZQ4MZHKNW";
+  var auth_token = "NTkxZmJkYmI0NGExZjk3ZGMzNzU0YTgzZjA5NTBm";
+  var plivo_number = "16623829952";
+
+  Parse.Cloud.httpRequest({
+    method: "POST",
+    headers: {'Content-Type': 'application/json',},
+    url: 'https://'+auth_id+':'+auth_token+'@api.plivo.com/v1/Account/'+auth_id+ '/Message/',
+    body: {
+      "src" : plivo_number,
+      "dst" : request.params.phonenumber,
+      "text" : request.params.messagebody
+    },
+    success: function(httpResponse) {
+      console.log(httpResponse.text);
+    },
+    error: function(httpResponse) {
+      console.error('Request failed with response code ' + httpResponse.status);
+    }
+  });
+});
