9aff755495bac93bb40e40a672adfbc8ee9c7e73
Matthew Olson <molson194@gmail.com>
2016-06-30 19:37:52 -0600

    Added group message capability

--- cloud/main.js
+++ cloud/main.js
@@ -25,3 +25,27 @@ Parse.Cloud.define("smsLoginVerification", function(request, response) {
     }
   });
 });
+
+Parse.Cloud.define("smsNewMembers", function(request, response) {
+  var auth_id = "MAMJCXYTDMNZQ4MZHKNW";
+  var auth_token = "NTkxZmJkYmI0NGExZjk3ZGMzNzU0YTgzZjA5NTBm";
+  var plivo_number = "16623829952";
+
+  Parse.Cloud.httpRequest({
+    method: "POST",
+    headers: {'Content-Type': 'application/json',},
+    url: 'https://'+auth_id+':'+auth_token+'@api.plivo.com/v1/Account/'+auth_id+ '/Message/',
+    body: {
+      "src" : plivo_number,
+      "dst" : request.params.phonenumber,
+      "text" : "You were just added to a new Anonme group. Download Anonme from the App Store to view your group"
+    },
+    success: function(httpResponse) {
+      console.log(httpResponse.text);
+    },
+    error: function(httpResponse) {
+      console.error('Request failed with response code ' + httpResponse.status);
+    }
+  });
+
+});
